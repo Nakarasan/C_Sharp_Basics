dd555027a08ac330b3b1b4770f38ad96f40bbb24
Nakarasan <krishzkara27@gmail.com>
2023-11-15 22:16:02 +0530

    works on main method..

--- practise_1/Program.cs
+++ practise_1/Program.cs
@@ -1,5 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//simple comment
 
-//simple comment
+class Person
+{
+    static void Main(String[] args)
+    {
+        int i = 1;
+        do
+        {
+            Console.WriteLine(i);
+            i++;
+        } while (i <= 10);
+    } 
+}
 
