db79505fffcaa99a529b66f6586743a3d97e9212
Nakarasan <krishzkara27@gmail.com>
2023-11-18 21:19:08 +0530

    vehicle class added

--- practise_1/Program.cs
+++ practise_1/Program.cs
@@ -1,15 +1,24 @@
 ﻿//simple comment
 
+using Microsoft.VisualBasic;
+using practise_1;
+
 class Person
 {
     static void Main(String[] args)
     {
-        int i = 1;
-        do
-        {
-            Console.WriteLine(i);
-            i++;
-        } while (i <= 10);
-    } 
+        //Vehicle car = new Vehicle();
+        //Console.WriteLine(car.FormatYear());
+        int area = GetArea(10, 5);
+
+        Console.WriteLine(area);
+        //.
+
+    }
+
+    public static int GetArea(int rectHeight, int rectWidth)
+    {
+        return rectHeight * rectWidth;
+    }
 }
 
---
+++ practise_1/Vehicle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practise_1
+{
+    internal class Vehicle
+    {
+        // return year as a string
+        public void OutputFormatedDate()
+        {
+            DateTime theDate = DateTime.Now;
+            Console.WriteLine(theDate.ToString("yyyy/MM/dd"));
+            if (theDate.Day == 25 && theDate.Month == 12)
+            {
+                string msg = "It's Christmas Day..";
+                Console.WriteLine(msg);
+                //return used for break the function...
+                return;
+            }
+            Console.WriteLine("It's a Normal Day..");
+        }
+
+        //for return integer year
+        public int FormatYear()
+        {
+            DateTime date = DateTime.Now;
+            int year = date.Year;
+
+            return year;
+        }
+
+        
+
+    }
+}
